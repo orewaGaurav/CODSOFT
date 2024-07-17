2a910962a4dfaa86a6dae93037519b94331a29cc
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-07-17 22:25:10 +0530

    calculator

---
+++ Calculator.py
@@ -0,0 +1,65 @@
+#Calculator using GUI
+#THIS IS MY CODE FOR CALCULATOR 
+from tkinter import *
+root=Tk()
+root.title("Hello it's me Calculator")
+root.geometry("700x500")
+root.config(bg="black")
+def startnow():
+    label=Label(root,text="Basic operation calculator",font=("arial",20),bg="black",fg="white")
+    label.grid(row=0,column=1)
+    def close():
+        exit()
+    def add():
+        a=float(entry1.get())
+        b=float(entry2.get())
+        result.config(text=f"Sum : {a+b}")
+    def sub():
+        a=float(entry1.get())
+        b=float(entry2.get())
+        result.config(text=f"Difference : {a-b}")
+    def mul():
+        a=float(entry1.get())
+        b=float(entry2.get())
+        result.config(text=f"Multiply : {a*b}")
+    def div():
+        a=float(entry1.get())
+        b=float(entry2.get())
+        if(b>0):
+            result.config(text=f"Division : {(a/b):.2f}")
+        else:
+            result.config(text="Divisor is zero")
+    label1=Label(root,text="Number 1",font=("arial",20),bg="black",fg="white")
+    label1.grid(row=2,column=0,padx=10,pady=5)
+    entry1=Entry(root,text="Enter first number",bg="white")
+    entry1.grid(row=2,column=1,padx=10,pady=5)
+    label2=Label(root,text="Number 2",font=("arial",20),bg="black",fg="white")
+    label2.grid(row=3,column=0,padx=10,pady=5)
+    entry2=Entry(root,text="Enter second number",bg="white")
+    entry2.grid(row=3,column=1,padx=10,pady=5)
+    addlabel=Label(root,text="Add",font=("arial",20),bg="black",fg="white")
+    addlabel.grid(row=4,column=0)
+    sublabel=Label(root,text="Substract",font=("arial",20),bg="black",fg="white")
+    sublabel.grid(row=5,column=0)
+    mullabel=Label(root,text="Multiply",font=("arial",20),bg="black",fg="white")
+    mullabel.grid(row=6,column=0)
+    divlabel=Label(root,text="Divide",font=("arial",20),bg="black",fg="white")
+    divlabel.grid(row=7,column=0)
+    addbutton=Button(root,text="+",command=add,font=("arial",20),bg="black",fg="white")
+    subbutton=Button(root,text="-",command=sub,font=("arial",20),bg="black",fg="white")
+    mulbutton=Button(root,text="*",command=mul,font=("arial",20),bg="black",fg="white")
+    divbutton=Button(root,text="/",command=div,font=("arial",20),bg="black",fg="white")
+
+    addbutton.grid(row=4,column=1)
+    subbutton.grid(row=5,column=1)
+    mulbutton.grid(row=6,column=1)
+    divbutton.grid(row=7,column=1)
+    button=Button(root,text="Close",command=close,font=("arial",20),bg="black",fg="white")
+    button.grid(row=0,column=4)
+    result=Label(root,text="Result : ",font=("arial",20),bg="black",fg="white")
+    result.grid(row=4,column=2)
+label=Label(root,text="Basic operation calculator",font=("arial",20),bg="black",fg="white")
+label.grid(row=0,column=1)
+start=Button(root,text="Start",font=("arial",20),bg="black",fg="white",command=startnow)
+start.grid(row=1,column=4)
+root.mainloop()
