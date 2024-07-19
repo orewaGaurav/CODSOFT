306874fe4088ba66b16767b5f5e1fa18a4549190
Gaurav Kumar <gauravkumar47967@gmail.com>
2024-07-19 22:16:46 +0530

    codes

---
+++ password_generator.py
@@ -0,0 +1,45 @@
+# This is my code for password generator 
+import string
+import random
+from tkinter import *
+root = Tk()
+
+s_lower=string.ascii_lowercase
+s_upper=string.ascii_uppercase
+s_digit=string.digits
+s_punctuation=string.punctuation
+l=[]
+l.extend(list(s_lower))
+l.extend(list(s_upper))
+l.extend(list(s_digit))
+l.extend(list(s_punctuation ))
+def stop():
+    exit()
+def generate():
+    new_list=l
+    random.shuffle(new_list)
+    length = int(entry.get())
+    if length>=8:
+        password ="".join(new_list[:length])
+        label3.config(text="Your password is : "+password)
+    else:
+        label3.config(text="Lenght should be 8 or greater")
+    button1=Button(root,text="Close",font=("arial",20),command=stop,bg="black",fg="white")
+    button1.grid(row=4,column=2,padx=20,pady=20)
+
+root.title("Passowrd Generator ")
+root.config(bg="black")
+root.geometry("1050x500")
+
+label1=Label(root,text="CodSoft Task",font=("arial",20),bg="black",fg="white")
+label1.grid(row=0,column=1,pady=20)
+
+label2=Label(root,text="Enter password length ",font=("arial",20),bg="black",fg="white")
+label2.grid(row=1,column=0,padx=20,pady=20)
+entry=Entry(root,font=("arial",20),bg="black",fg="white")
+entry.grid(row=1,column=1,padx=20,pady=20)
+button=Button(root,text="Generate password",font=("arial",20),command=generate,bg="black",fg="white")
+button.grid(row=2,column=1,padx=20,pady=20)
+label3=Label(root,text="",font=("arial",20),bg="black",fg="white")
+label3.grid(row=3,column=1,padx=20,pady=20)
+root.mainloop()
---
+++ rock_paper_scissors.py
@@ -0,0 +1,63 @@
+#This is my code for Rock Paper Scissor Game
+from tkinter import *
+import random
+choice_list=["rock","paper","scissor"]
+def new():
+    #Game Rule
+    def rules(a):
+        b=random.choice(choice_list)
+        if a==b:
+            label2.config(text="Computer's choice is also "+b)
+            label3.config(text="Tie !")
+        elif a=="rock" and b=="paper":
+            label2.config(text="Computer's choice is "+b)
+            label3.config(text="Computer Won !")
+        elif a=="paper" and b=="scissor":
+            label2.config(text="Computer's choice is "+b)
+            label3.config(text="Computer Won !")
+        elif a=="scissor" and b=="rock":
+            label2.config(text="Computer's choice is "+b)
+            label3.config(text="Computer Won !")
+        else:
+            label2.config(text="Computer's choice is "+b)
+            label3.config(text="You Won !")
+        reset=Button(root,text="New Game",bg="black",fg="white",font=("arial",30),command=new)
+        reset.grid(row=8,column=3,pady=10)
+    #Choices
+    def choice_1():
+        a="rock"
+        rules(a)
+    def choice_2():
+        a="paper"
+        rules(a)
+    def choice_3():
+        a="scissor"
+        rules(a)
+    def st():
+        label1.config(text="Welcome "+entry.get())
+
+    #GUI
+    root=Tk()
+    root.title("Rock Paper Scissor Game ")
+    root.config(bg="black")
+    root.geometry("1300x900")
+    label=Label(root,text="Enter your name",bg="black",fg="white",font=("arial",30))
+    label.grid(row=1,column=1,pady=20)
+    entry=Entry(root,font=("arial",30),bg="black",fg="white")
+    entry.grid(row=2,column=1,pady=50)
+    start=Button(root,text="Start",bg="black",fg="white",font=("arial",30),command=st)
+    start.grid(row=3,column=1)
+    label1=Label(root,text="",font=("arial",30),bg="black",fg="white")
+    label1.grid(row=4,column=1,pady=10)
+    choice1=Button(root,text="Rock",bg="black",fg="white",font=("arial",30),command=choice_1,height=4,width=8)
+    choice1.grid(row=5,column=0,padx=10,pady=50)
+    choice2=Button(root,text="Paper",bg="black",fg="white",font=("arial",30),command=choice_2,height=4,width=8)
+    choice2.grid(row=5,column=1,padx=10,pady=50)
+    choice3=Button(root,text="Scissors",bg="black",fg="white",font=("arial",30),command=choice_3,height=4,width=8)
+    choice3.grid(row=5,column=2,padx=10,pady=50)
+    label2=Label(root,text="",bg="black",fg="white",font=("arial",30))
+    label2.grid(row=6,column=1)
+    label3=Label(root,text="",bg="black",fg="white",font=("arial",30))
+    label3.grid(row=7,column=1)
+    root.mainloop()
+new()
